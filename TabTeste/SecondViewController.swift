d13394e86caae90365393d797fdbbabaf2dee6a2
Laercio Silva Ferracini <lasicin@500010338861-nb.local>
2019-09-10 19:10:24 -0300

    Initial Commit

---
+++ TabTeste/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabTeste
+//
+//  Created by Laercio Silva Ferracini on 10/09/19.
+//  Copyright © 2019 Laercio Silva Ferracini. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
